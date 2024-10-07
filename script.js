3b3f70622f118318a7a9a0ddb7fcb3b927fd7f7e
AyeshaRahman2002 <63969920+AyeshaRahman2002@users.noreply.github.com>
2024-10-08 00:59:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-// script.js
+// You can add any JavaScript you want to use for animations or interactivity here.
+// For example, a simple alert when the contact form is submitted.
 
-// Future JavaScript functionality can go here
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+});
