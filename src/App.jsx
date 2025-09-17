75ded9e2f9ccec19ab0220de1fb3bcaa629b97ee
AyeshaRahman2002 <63969920+AyeshaRahman2002@users.noreply.github.com>
2025-09-17 20:16:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ContactForm from "./components/ContactForm";
 import Blog1 from "./components/blog1.jsx";
 import Blog2 from "./components/blog2.jsx";
 import Blog3 from "./components/blog3.jsx";
-import Resume from "../assets/Resume-Ayesha.pdf";
+import Resume from "./assets/Resume-Ayesha.pdf";
 
 const LINKS = { 
   resume: Resume, 
